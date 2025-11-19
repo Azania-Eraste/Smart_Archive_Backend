0c85bb5eaebe8bc1abff7a4de7dc1e7ca9662ceb
Azania-Eraste <kouadioazania@gmail.com>
2025-11-19 22:00:37 +0100

    config media

--- SmartArchive_Backend/urls.py
+++ SmartArchive_Backend/urls.py
@@ -1,23 +1,16 @@
-"""
-URL configuration for SmartArchive_Backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+# systeme_archivage/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # --- NOS API ---
+    path("api/v1/comptes/", include("comptes.urls")),
+    path("api/v1/etablissement/", include("etablissement.urls")),
 ]
+
+# Pour servir les fichiers uploadés (Images, PDF) en mode développement
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
