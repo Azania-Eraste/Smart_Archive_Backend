b0e52b6e0021bf6bb83e619b2da39359baa63194
Azania-Eraste <kouadioazania@gmail.com>
2025-11-20 00:23:11 +0100

    modification du settings

--- SmartArchive_Backend/settings.py
+++ SmartArchive_Backend/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "jazzmin",
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
