c5eb20c82613b4b092ee0cfe612f022f10016fa3
Azania-Eraste <kouadioazania@gmail.com>
2025-11-19 23:43:21 +0100

    changement du nom de uls a urls dans compte et ajout de inscription et pedagogie dans urls generale

--- SmartArchive_Backend/urls.py
+++ SmartArchive_Backend/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("api/comptes/", include("comptes.urls")),
     path("api/etablissement/", include("etablissement.urls")),
     path("api/dossiers/", include("dossiers.urls")),
+    path("api/inscriptions/", include("inscriptions.urls")),
+    path("api/pedagogie/", include("pedagogie.urls")),
 ]
 
 # Pour servir les fichiers uploadés (Images, PDF) en mode développement
--- comptes/serializers.py
+++ comptes/serializers.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 
 from .models import (  # Si Eleve est dans comptes, sinon ajustez
     Educateur,
-    Eleve,
     Parent,
     Professeur,
 )
